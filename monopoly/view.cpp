825804db7e59759d6698efad1d5f0ce711e1ec38
Marina Zemlyanaya <zemlyanaya10@gmail.com>
2019-03-03 00:55:36 +0200

    sellShop

--- monopoly/view.cpp
+++ monopoly/view.cpp
@@ -6,8 +6,14 @@ int View::textMenu() {
 	std::cout << "What do u want to do? " << std::endl;
 	std::cin >> what;
 	if (what == 'e') return 0;
-	if (what == 's') {
+	if (what == 's') {//stroit domik
 		int num;
+		std::cin >> num;
+		return num;
+	}
+	if (what == 'p') {//prodat domik
+		int num;
+		std::cout << "Print number of card with -\n";
 		std::cin >> num;
 		return num;
 	}
